8b38ba44c2ea23ecdef10506f59a2537245c6b73
sankhaKarunasekara <www.numbers.lk@gmail.com>
2021-01-02 16:15:46 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,284 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="bg-grey-100">
+    <div
+      id="app"
+      class="h-screen w-screen bg-indigo-400 overflow-hidden absolute flex items-center"
+    >
+      <div
+        class="w-screen h-64 absolute top-0 opacity-50 left-0 -my-40 -mx-64 bg-indigo-300 rounded-full"
+      ></div>
+      <div class="w-64 h-64 -mx-32 bg-indigo-300 opacity-50 rounded-full"></div>
+      <div
+        class="w-64 h-64 ml-auto relative opacity-50 -mr-32 bg-indigo-300 rounded-full"
+      ></div>
+      <div
+        class="w-screen h-64 absolute opacity-50 bottom-0 right-0 -my-40 -mx-64 bg-indigo-300 rounded-full"
+      ></div>
+    </div>
+
+    <div class="container mx-auto h-screen py-16 px-8 relative">
+      <div
+        class="flex w-full rounded-lg h-full lg:overflow-hidden overflow-auto lg:flex-row flex-col shadow-2xl"
+      >
+        <div class="lg:w-1/2 bg-white text-gray-800 flex flex-col">
+          <div class="p-8 shadow-md relative bg-white">
+            <!-- <div class="flex items-center">
+              <img
+                src="https://randomuser.me/api/portraits/men/1.jpg"
+                class="w-10 h-10 block rounded object-cover object-top"
+              />
+              <div class="text-indigo-600 font-medium ml-3">
+                Holden Caulfield
+              </div>
+              <button
+                class="bg-indigo-100 text-indigo-400 ml-auto w-8 h-8 flex items-center justify-center rounded"
+              >
+                <svg
+                  stroke="currentColor"
+                  class="w-4 h-4"
+                  viewBox="0 0 24 24"
+                  stroke-width="2.2"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path
+                    d="M18 8A6 6 0 006 8c0 7-3 9-3 9h18s-3-2-3-9M13.73 21a2 2 0 01-3.46 0"
+                  />
+                </svg>
+              </button>
+            </div> -->
+            <h1 class="font-medium text-lg mt-6">
+              Re Registration Issues List
+            </h1>
+            <p class="text-gray-600 text-sm">
+              search and find a issue and click the check box.
+            </p>
+            <div class="mt-6 flex">
+              <button
+                class="bg-indigo-500 text-white py-2 text-sm px-3 rounded focus:outline-none"
+              >
+                New Issue
+              </button>
+              <div class="relative ml-auto flex-1 pl-8 sm:block hidden">
+                <input
+                  placeholder="Search"
+                  type="text"
+                  v-model="search"
+                  class="w-full border rounded border-gray-400 h-full focus:outline-none pl-4 pr-8 text-gray-700 text-sm text-gray-500"
+                />
+                <svg
+                  stroke="currentColor"
+                  class="w-4 h-4 absolute right-0 top-0 mt-3 mr-2 text-gray-500"
+                  stroke-width="2"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  viewBox="0 0 24 24"
+                >
+                  <circle cx="11" cy="11" r="8" />
+                  <path d="M21 21l-4.35-4.35" />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div class="overflow-auto flex-grow">
+            <div v-for="tab in validationQuestions.tabs" :key="tab.tab_name">
+              <div v-for="section in tab.sections" :key="section.name">
+                <div v-for="item in section.items" :key="item.name">
+                  <ListItem
+                    v-if="isSearch(item, tab.tab_name, section.name)"
+                    :tab-name="tab.tab_name"
+                    :section-name="section.name"
+                    :item="item"
+                    @record-checkbox-value="recordCheckboxValue"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="lg:w-1/2 bg-indigo-600 text-white flex flex-col">
+          <div class="p-8 bg-indigo-700 flex items-center">
+            <!-- <img
+              src="https://pbs.twimg.com/profile_images/1000050491970260993/FJkauyEa.jpg"
+              class="w-16 h-16 mr-4 object-top object-cover rounded"
+            /> -->
+            <div class="mr-auto">
+              <h1 class="text-xl leading-none mb-1">Generated Email</h1>
+              <h2 class="text-indigo-400 text-sm">
+                you can copy and paste it in the email body
+              </h2>
+            </div>
+            <!-- <button
+              class="bg-indigo-600 text-white py-2 text-sm px-3 rounded focus:outline-none"
+            >
+              New Task
+            </button> -->
+          </div>
+          <div class="p-8 flex flex-1 items-start overflow-auto">
+            <div class="flex-1">
+              <div
+                class="h-screen w-full bg-transparent text-black mt-2 justify-between"
+              >
+                <textarea
+                  v-model="mailtextbox"
+                  placeholder="Message..."
+                  class="w-full p-2 font-serif focus:outline-none focus:ring focus:border-blue-300 border-blue-100 border shadow text-sm rounded-lg resize-none bg-white h-full"
+                ></textarea>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="fixed h-screen right-0 top-0 items-center flex">
+      <div
+        class="p-2 bg-white border-l-4 border-t-4 border-b-4 border-indigo-400 inline-flex items-center rounded-tl-lg shadow-2xl rounded-bl-lg z-10 flex-col"
+      >
+        <button
+          class="bg-gray-500 w-5 h-5 rounded-full mb-2 outline-none focus:outline-none"
+          theme-button="gray"
+        ></button>
+        <button
+          class="bg-red-500 w-5 h-5 rounded-full mb-2 outline-none focus:outline-none"
+          theme-button="red"
+        ></button>
+        <button
+          class="bg-green-500 w-5 h-5 rounded-full mb-2 outline-none focus:outline-none"
+          theme-button="green"
+        ></button>
+        <button
+          class="bg-blue-500 w-5 h-5 rounded-full mb-2 outline-none focus:outline-none"
+          theme-button="blue"
+        ></button>
+        <button
+          class="bg-indigo-500 w-5 h-5 rounded-full mb-2 outline-none focus:outline-none"
+          theme-button="indigo"
+        ></button>
+        <button
+          class="bg-purple-500 w-5 h-5 rounded-full mb-2 outline-none focus:outline-none"
+          theme-button="purple"
+        ></button>
+        <button
+          class="bg-pink-500 w-5 h-5 rounded-full outline-none focus:outline-none"
+          theme-button="pink"
+        ></button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListItem from "./components/ListItem.vue";
+import validationQuestions from "@/data/validation.json";
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
-  }
-}
+    ListItem,
+  },
+
+  mounted() {
+    const allElements = document.querySelectorAll(
+      '*:not([theme-button]):not([class*="gray"])'
+    );
+    const themeButtons = document.querySelectorAll("[theme-button]");
+    const escapeRegExp = (string) =>
+      string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+    const replaceAll = (str, term, replacement) =>
+      str.replace(new RegExp(escapeRegExp(term), "g"), replacement);
+    let currTheme = "indigo";
+
+    const changeTheme = (theme) => {
+      allElements.forEach((element) => {
+        if (element.getAttribute("class") !== null) {
+          const newClasses = replaceAll(
+            element.getAttribute("class"),
+            currTheme,
+            theme
+          );
+          element.setAttribute("class", newClasses);
+        }
+      });
+      currTheme = theme;
+    };
+
+    themeButtons.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        changeTheme(e.target.getAttribute("theme-button"));
+      });
+    });
+  },
+
+  data() {
+    return {
+      validationQuestions: validationQuestions,
+      checkedItemsList: [],
+      mailtextbox: "",
+      search: "",
+    };
+  },
+
+  methods: {
+    recordCheckboxValue: function ({
+      item,
+      tabName,
+      sectionName,
+      checkboxValue,
+    }) {
+      if (checkboxValue) {
+        this.checkedItemsList.push({ tabName, sectionName, ...item });
+      } else {
+        this.checkedItemsList = this.checkedItemsList.filter(
+          (checkedItem) =>
+            !this.isTheSameItem(checkedItem, item, tabName, sectionName)
+        );
+      }
+      this.mailtextbox = "";
+      this.generateListItemsString();
+    },
+
+    generateListItemsString: function () {
+      let i = 0;
+      for (i = 0; i < this.checkedItemsList.length; i++)
+        this.mailtextbox = this.mailtextbox.concat(
+          this.generateAListItem(i, this.checkedItemsList[i])
+        );
+    },
+
+    isTheSameItem(item1, item2, tabName, sectionName) {
+      if (
+        item1.no == item2.no &&
+        item1.tabName == tabName &&
+        item1.sectionName == sectionName
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+
+    generateAListItem(i, item) {
+      let text = `${i + 1}. ${item.message}\n`;
+      return text;
+    },
+
+    isSearch(item, tabName, sectionName) {
+      return item.message
+        .concat(` ${tabName} ${sectionName}`)
+        .toLowerCase()
+        .includes(this.search.toLowerCase());
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,93 @@
+<template>
+  <div
+    class="bg-gray-100 px-8 py-4 flex items-center border-b border-gray-300 hover:bg-gray-200 cursor-pointer"
+    @click="changeTheCheckBoxValue()"
+  >
+    <input
+      type="checkbox"
+      class="form-checkbox h-5 w-5 text-blue-600"
+      v-model="checkboxValue"
+    />
+    <div class="flex ml-4">
+      <img
+        src="../assets/images/critical.png"
+        class="w-10 h-10 object-cover rounded object-top"
+      />
+      <div class="flex flex-col pl-4">
+        <h2 class="font-normal text-sm">{{ item.condition }}</h2>
+        <h3 class="text-gray-700 text-sm">
+          {{ sectionName }}
+        </h3>
+        <div
+          class="uppercase text-xs text-gray-600 leading-tight font-semibold"
+        >
+          {{ tabName }}
+        </div>
+      </div>
+    </div>
+    <!-- <button
+      class="text-gray-500 flex items-center text-sm focus:outline-none rounded ml-auto py-2 leading-none"
+    >
+      <svg
+        stroke="currentColor"
+        stroke-width="2"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="w-4 h-4 mr-2"
+        viewBox="0 0 24 24"
+      >
+        <circle cx="12" cy="12" r="3" />
+        <path
+          d="M19.4 15a1.65 1.65 0 00.33 1.82l.06.06a2 2 0 010 2.83 2 2 0 01-2.83 0l-.06-.06a1.65 1.65 0 00-1.82-.33 1.65 1.65 0 00-1 1.51V21a2 2 0 01-2 2 2 2 0 01-2-2v-.09A1.65 1.65 0 009 19.4a1.65 1.65 0 00-1.82.33l-.06.06a2 2 0 01-2.83 0 2 2 0 010-2.83l.06-.06a1.65 1.65 0 00.33-1.82 1.65 1.65 0 00-1.51-1H3a2 2 0 01-2-2 2 2 0 012-2h.09A1.65 1.65 0 004.6 9a1.65 1.65 0 00-.33-1.82l-.06-.06a2 2 0 010-2.83 2 2 0 012.83 0l.06.06a1.65 1.65 0 001.82.33H9a1.65 1.65 0 001-1.51V3a2 2 0 012-2 2 2 0 012 2v.09a1.65 1.65 0 001 1.51 1.65 1.65 0 001.82-.33l.06-.06a2 2 0 012.83 0 2 2 0 010 2.83l-.06.06a1.65 1.65 0 00-.33 1.82V9a1.65 1.65 0 001.51 1H21a2 2 0 012 2 2 2 0 01-2 2h-.09a1.65 1.65 0 00-1.51 1z"
+        />
+      </svg>
+      Edit
+    </button> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListItem",
+  props: {
+    item: {
+      type: Object,
+      default: () => {},
+    },
+    tabName: {
+      type: String,
+      default: "",
+    },
+    sectionName: {
+      type: String,
+      default: "",
+    },
+  },
+
+  data() {
+    return {
+      checkboxValue: false,
+    };
+  },
+  methods: {
+    updateValue: function () {
+      this.$emit("record-checkbox-value", {
+        item: this.item,
+        tabName: this.tabName,
+        sectionName: this.sectionName,
+        checkboxValue: this.checkboxValue,
+      });
+    },
+
+    changeTheCheckBoxValue: function () {
+      this.checkboxValue = !this.checkboxValue;
+      this.updateValue();
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
